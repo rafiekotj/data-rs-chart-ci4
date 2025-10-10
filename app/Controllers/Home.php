addffafce2a92599c30fefcc0c223a3157f84c79
rafiekotj <rafiekhana@gmail.com>
2025-10-10 08:23:01 +0700

    add: testing for supabase

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,18 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function dbtest()
+    {
+        try {
+            $db = \Config\Database::connect();
+            if ($db->connID) {
+                echo "✅ Koneksi ke Supabase berhasil!";
+            } else {
+                echo "❌ Gagal konek ke database.";
+            }
+        } catch (\Throwable $e) {
+            echo "⚠️ Error: " . $e->getMessage();
+        }
+    }
 }
