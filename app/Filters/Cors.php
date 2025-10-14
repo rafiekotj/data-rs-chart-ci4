c1da829728872acfcfba6028ecae4c0158294316
rafiekotj <rafiekhana@gmail.com>
2025-10-14 08:32:29 +0700

    fix: cors for render

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -10,13 +10,13 @@ class Cors implements FilterInterface
 {
   public function before(RequestInterface $request, $arguments = null)
   {
-    // Header dasar CORS
+    // Tambahkan header CORS
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
     // Tangani preflight (OPTIONS)
-    if ($request->getMethod(true) === 'OPTIONS') {
+    if (strtoupper($request->getMethod()) === 'OPTIONS') {
       $response = service('response');
       $response->setStatusCode(200);
       $response->setBody('OK');
